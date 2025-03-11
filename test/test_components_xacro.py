b4a51d3525948267efbfb7f5c4971f55c337ff0c
Jakub Delicat <jakub.delicat@husarion.com>
2025-03-11 11:56:32 +0000

    Added DEV kits | applied tests
    Signed-off-by: Jakub Delicat <jakub.delicat@husarion.com>

--- test/test_components_xacro.py
+++ test/test_components_xacro.py
@@ -27,6 +27,7 @@ xacro_path = os.path.join(ros_components_description, "test/component.urdf.xacro
 
 # Type: [device_namespace, link_name, sensor_link_name, sensor_name, default_device_namespace]
 components_types_with_names = {
+    "DEV01": ["", "dev01_link", "", "", ""],
     "LDR01": ["slamtec_rplidar_s1", "laser", "laser", "slamtec_rplidar_sensor", ""],
     "LDR06": ["slamtec_rplidar_s3", "laser", "laser", "slamtec_rplidar_sensor", ""],
     "LDR10": ["ouster_os0_32", "os_lidar", "os_lidar", "ouster_os0_32_sensor", ""],
@@ -176,8 +177,14 @@ class ComponentsYamlParseUtils:
 def test_all_good_single_components(tmpdir_factory):
     for type_name, value in components_types_with_names.items():
         device_namespace = value[0]
-        dir = tmpdir_factory.mktemp(device_namespace)
-        components_config_path = dir.join(device_namespace + "_test_components.yaml")
+        folder_name = device_namespace
+
+        if "DEV" in type_name :
+            folder_name = type_name
+
+
+        dir = tmpdir_factory.mktemp(folder_name)
+        components_config_path = dir.join(folder_name + "_test_components.yaml")
 
         utils = ComponentsYamlParseUtils(str(components_config_path))
         components = {
